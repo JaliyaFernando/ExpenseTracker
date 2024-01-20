f3a87df5e4f94c5c7d227a756b40f8320afa3856
Hiran Aloka <aloka@limarktech.com>
2024-01-20 08:47:06 +0530

    added UI design

--- Frontend/expense_tracker_app/src/app/constants/APIs.js
+++ Frontend/expense_tracker_app/src/app/constants/APIs.js
@@ -1,6 +1,7 @@
 module.exports = {
     TRANSACTIONS_BASE_URL: 'http://localhost:8080/transactions',
     CATEGORIES_BASE_URL: 'http://localhost:8080/categories',
+    SUMMARY_BASE_URL: 'http://localhost:8080/summary',
     transaction: {
         RECENT: '/recent/',
         ADD_NEW_RECURRING: '/recurring',
